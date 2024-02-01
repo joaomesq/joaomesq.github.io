a33c53c3296955aa2fed5f239b14a26cb0624205
João Mesquita <92105371+joaomesq@users.noreply.github.com>
2024-02-01 13:25:29 -0300

    Public portfolio
    DEMO portfolio

---
+++ js/efeitos.js
@@ -0,0 +1,16 @@
+let ver_portfolio = document.getElementById("btn_portfolio");
+
+function mostrar_seccoes(){
+         document.getElementById("seccoes").style.display="unset";
+         document.getElementById("seccoes").style.animationDelay=".4s";
+}
+
+function carregar(){
+	ver_portfolio.addEventListener("click", mostrar_seccoes);
+	document.getElementById("link_sobre").addEventListener("click", mostrar_seccoes);
+	document.getElementById("link_projetos").addEventListener("click", mostrar_seccoes);
+	document.getElementById("link_habilidades").addEventListener("click", mostrar_seccoes);
+	document.getElementById("link_contato").addEventListener("click", mostrar_seccoes);
+}
+
+window.addEventListener("load", carregar);
