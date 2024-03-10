27bcd81120cf59d3547afdae9c088d220f2e3281
João Mesquita <92105371+joaomesq@users.noreply.github.com>
2024-03-10 19:16:52 -0300

    Add files via upload
    Adição da secção para projetos

--- js/efeitos.js
+++ js/efeitos.js
@@ -1,13 +1,13 @@
  let ver_portfolio = document.getElementById("btn_portfolio");
 
 function mostrar_seccoes(){
-         document.getElementById("seccoes").style.display="unset";
-         document.getElementById("seccoes").style.animationDelay=".4s";
+         document.getElementById("seccoes").style.display="flex";
 }
 
 function carregar(){
 	ver_portfolio.addEventListener("click", mostrar_seccoes);
 	document.getElementById("link_sobre").addEventListener("click", mostrar_seccoes);
+	document.getElementById("link_projetos").addEventListener("click", mostrar_seccoes);
 	document.getElementById("link_habilidades").addEventListener("click", mostrar_seccoes);
 	document.getElementById("link_contato").addEventListener("click", mostrar_seccoes);
 }
